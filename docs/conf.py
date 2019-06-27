0f3c531a7e07ea4bf70c1618e1bf8debd6499af9
William F. Broderick <billbrod@gmail.com>
2019-06-27 17:19:06 -0400

    adds updates for pooling docs
     - updates the README to include the sphinx-apidoc line
    
     - includes some new Napoleon settings
    
     - updates plenoptic.simulate.rst (happened automatically after
       re-calling sphinx-apidoc) to include pooling

--- docs/conf.py
+++ docs/conf.py
@@ -69,6 +69,20 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
 
+# Napoleon settings
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
