5b2e1519b30f17c7c808c748e04cdfe63125f714
William F. Broderick <billbrod@gmail.com>
2019-08-15 14:24:19 -0700

    adds normalization
    adds two new helper functions to non_linearities: generate_norm_stats
    and zscore_stats. generate_norm_stats takes a model and a directory,
    runs the model on every image in that directory, and saves the
    resulting statistics. if the model has a to_normalize attribute, we
    save those attributes, otherwise we save the value returned by
    model(images).
    
    this is to be used with the zscore_stats function, which takes a model
    and the dictionary generated above, and zscores them (using the mean
    and std across images). caveat here is there's no way I can abstract
    away when it should be called, so that's up to the user

--- plenoptic/simulate/canonical_computations/__init__.py
+++ plenoptic/simulate/canonical_computations/__init__.py
@@ -3,4 +3,6 @@ from .linear_nonlinear import Linear_Nonlinear
 from .laplacian_pyramid import Laplacian_Pyramid
 from .steerable_pyramid_freq import Steerable_Pyramid_Freq
 from .pooling import PoolingWindows
-from .non_linearities import rectangular_to_polar_dict, local_gain_control
+from .non_linearities import (rectangular_to_polar_dict, local_gain_control, generate_norm_stats,
+                              zscore_stats)
+
