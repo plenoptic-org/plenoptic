0c6969349fb18fb217eec5ef4a69655d01b79415
William F. Broderick <billbrod@gmail.com>
2020-05-08 17:05:52 -0400

    fixes FrontEnd issue
    Front_End had a problem with torch 1.5. previously, torch would
    convert np.float64 to torch.float32; now, it converts them to
    torch.float64. so need to explicitly set them to float32
    
    also cleans up a bit

--- plenoptic/simulate/models/frontend.py
+++ plenoptic/simulate/models/frontend.py
@@ -48,26 +48,34 @@ class Front_End(nn.Module):
         parasol off
     """
     def __init__(self, disk_mask=False):
-        super(Front_End, self).__init__()
+        super().__init__()
 
-        self.filts = np.load(dirname + '/front_end_filters.npy')
-        self.scals = np.load(dirname + '/front_end_scalars.npy')
+        filts = np.load(dirname + '/front_end_filters.npy')
+        scals = np.load(dirname + '/front_end_scalars.npy')
 
         linear = nn.Conv2d(in_channels=1, out_channels=2, kernel_size=31, bias=False)
-        linear.weight = nn.Parameter(torch.tensor((self.filts[0], self.filts[3]),
+        linear.weight = nn.Parameter(torch.tensor((filts[0], filts[3]),
                                      dtype=torch.float32).unsqueeze(1))
 
         luminance = nn.Conv2d(in_channels=1, out_channels=2, kernel_size=31,
                               bias=False)
-        luminance.weight = nn.Parameter(torch.tensor((self.filts[1], self.filts[4]),
+        luminance.weight = nn.Parameter(torch.tensor((filts[1], filts[4]),
                                         dtype=torch.float32).unsqueeze(1))
 
         contrast = nn.Conv2d(in_channels=2, out_channels=2, kernel_size=31,
                              groups=2, bias=False)
-        contrast.weight = nn.Parameter(torch.tensor((self.filts[2], self.filts[5]),
+        contrast.weight = nn.Parameter(torch.tensor((filts[2], filts[5]),
                                        dtype=torch.float32).unsqueeze(1))
 
-        pad = nn.ReflectionPad2d(self.filts.shape[-1]//2)
+        pad = nn.ReflectionPad2d(filts.shape[-1]//2)
+
+        self.luminance_scals = torch.tensor((scals[0], scals[2]), dtype=torch.float32)
+        self.luminance_scals = self.luminance_scals.view((1, 2, 1, 1))
+
+        self.contrast_scals = torch.tensor((scals[1], scals[3]), dtype=torch.float32)
+        self.contrast_scals = self.luminance_scals.view((1, 2, 1, 1))
+
+        del filts, scals
 
         self.linear = transforms.Compose([pad, linear])
         self.luminance = transforms.Compose([pad, luminance])
@@ -78,14 +86,12 @@ class Front_End(nn.Module):
 
     def luminance_normalization(self, x):
 
-        s = torch.tensor((self.scals[0], self.scals[2])).view((1, 2, 1, 1))
-
+        s = self.luminance_scals
         return torch.div(self.linear(x), (1 + s * self.luminance(x)))
 
     def contrast_normalization(self, x):
 
-        s = torch.tensor((self.scals[1], self.scals[3])).view((1, 2, 1, 1))
-
+        s = self.contrast_scals
         return torch.div(x, 1 + s * torch.sqrt(1e-10 + self.contrast(x ** 2)))
 
     def forward(self, x):
