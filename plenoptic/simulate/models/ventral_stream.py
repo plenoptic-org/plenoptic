a35aadc6435029fba8447a013cd43bd910f4d933
William F. Broderick <billbrod@gmail.com>
2020-06-15 16:14:38 -0500

    moves mean_luminance back to coarsest scale

--- plenoptic/simulate/models/ventral_stream.py
+++ plenoptic/simulate/models/ventral_stream.py
@@ -1464,7 +1464,7 @@ class PrimaryVisualCortex(VentralModel):
         self.order = order
         self.complex_steerable_pyramid = Steerable_Pyramid_Freq(img_res, self.num_scales,
                                                                 self.order, is_complex=True)
-        self.scales = []
+        self.scales = ['mean_luminance']
         if half_octave_pyramid:
             self.half_octave_img_res = [int(round(i / np.sqrt(2))) for i in img_res]
             # want this to be even. for plotting purposes, the more
@@ -1497,7 +1497,6 @@ class PrimaryVisualCortex(VentralModel):
         if self.include_highpass:
             self.scales += ['residual_highpass']
             self.to_normalize += ['residual_highpass']
-        self.scales += ['mean_luminance']
         self.normalize_dict = normalize_dict
 
     def to(self, *args, do_windows=True, **kwargs):
