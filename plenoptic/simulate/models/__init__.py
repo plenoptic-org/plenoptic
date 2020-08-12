4c4dc8566bf3795a146968f2fa132db292dd7802
pehf <pef246@nyu.edu>
2020-08-12 15:47:02 -0400

    clean init file

--- plenoptic/simulate/models/__init__.py
+++ plenoptic/simulate/models/__init__.py
@@ -3,4 +3,3 @@ from .texture_statistics import Texture_Statistics
 from .spectral import Spectral
 from .ventral_stream import RetinalGanglionCells, PrimaryVisualCortex
 from .steerable_gaincontrol import Steerable_GainControl
-from .V2 import V2
