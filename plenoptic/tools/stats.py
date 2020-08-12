ed3c8deb9d466dd24cbf0778d42c621f1a3310af
pehf <pef246@nyu.edu>
2020-08-12 15:44:02 -0400

    allow skew and kurtosis to operate on desired set of dims

--- plenoptic/tools/stats.py
+++ plenoptic/tools/stats.py
@@ -1,17 +1,17 @@
 import torch
 
 
-def kurtosis(x):
+def skew(x, dim=None, keepdim=False):
+    r"""sample estimate of 'x' *asymmetry* about its mean
+    """
+    return torch.mean((x - x.mean(dim=dim, keepdim=keepdim)).pow(3), dim=dim, keepdim=keepdim)/(x.var(dim=dim, keepdim=keepdim).pow(1.5))
+
+
+def kurtosis(x, dim=None, keepdim=False):
     r"""sample estimate of 'x' *tailedness* (outliers)
     kurtosis of univariate noral is 3
     smaller than 3: *platykurtic* (eg. uniform distribution)
     greater than 3: *leptokurtic* (eg. Laplace distribution)
     """
     # implementation is only for real components
-    return torch.mean(torch.abs(x-x.mean()).pow(4))/(x.var().pow(2))
-
-
-def skew(x):
-    r"""sample estimate of 'x' *asymmetry* about its mean
-    """
-    return torch.mean((x-x.mean()).pow(3))/(x.var().pow(1.5))
+    return torch.mean(torch.abs(x - x.mean(dim=dim, keepdim=keepdim)).pow(4), dim=dim, keepdim=keepdim)/(x.var(dim=dim, keepdim=keepdim).pow(2))
