4d4494d1a687ea6bb8fc9f8cf681c59e764a5e5e
pehf <pef246@nyu.edu>
2020-08-12 15:45:12 -0400

    adding a function to penalize the range of the synthesized object during optimization

--- plenoptic/tools/fit.py
+++ plenoptic/tools/fit.py
@@ -1,6 +1,33 @@
 import torch
 
 
+def penalize_range(x, allowed_range=(0, 1)):
+    r"""penalize values outside of allowed_range
+    instead of clamping values to exactly fall in a range, this provides
+    a 'softer' way of doing it, by imposing a quadratic penalty on any
+    values outside the allowed_range. All values within the
+    allowed_range have a penalty of 0
+    Parameters
+    ----------
+    x : torch.Tensor
+        the tensor to penalize. the synthesized image.
+    allowed_range : tuple, optional
+        2-tuple of values giving the (min, max) allowed values
+    kwargs :
+        ignored, only present to absorb extra arguments
+    Returns
+    -------
+    penalty : torch.float
+        penalty for values outside range
+    """
+    # the indexing should flatten it
+    below_min = x[x < allowed_range[0]]
+    below_min = torch.pow(below_min - allowed_range[0], 2)
+    above_max = x[x > allowed_range[1]]
+    above_max = torch.pow(above_max - allowed_range[1], 2)
+    return torch.sum(torch.cat([below_min, above_max]))
+
+
 def pretty_print(i, max_iter, t, loss, g_norm,lr=[]):
     if lr==[]:
         print('iter', i + 1, '/', max_iter,
@@ -14,19 +41,3 @@ def pretty_print(i, max_iter, t, loss, g_norm,lr=[]):
               '\ttime',      round(t, 3),
               '\tgrad norm', round(g_norm, 3),
               '\tlr', round(lr,6))
-
-
-def stretch(z):
-    """Soft rescaling, the inverse of squish used at initialization
-    from (-1, 1) to (-inf,inf) - avoid infinity by clamping
-    tanh(log((1+x)/(1-x))/2) = identity, for x in (-1,1)
-
-    Parameters
-    ----------
-    z: `torch.Tensor`
-        Tensor whose values will be stretched.
-
-    """
-    epsilon = 10e-8
-    z = torch.clamp(z, -1 + epsilon, 1 - epsilon)
-    return torch.log((1 + z) / (1 - z)) / 2
