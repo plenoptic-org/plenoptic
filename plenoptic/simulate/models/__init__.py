c8f4dd54267c06ea0e2e4b31fcfa2e6f7676a07d
Nikhil Parthasarathy <nikparth@gmail.com>
2020-08-12 23:49:49 -0400

    fix model imports in init

--- plenoptic/simulate/models/__init__.py
+++ plenoptic/simulate/models/__init__.py
@@ -1,6 +1,2 @@
 from .frontend import Front_End
-from .texture_statistics import Texture_Statistics
-from .spectral import Spectral
-from .ventral_stream import PooledRGC, PooledV1
-from .steerable_gaincontrol import Steerable_GainControl
-from .V2 import V2
+from .ventral_stream import PooledVentralStream, PooledV1, PooledRGC
