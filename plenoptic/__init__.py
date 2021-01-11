ebf014aeafd820e23673cc7d650578719be3e668
pehf <pef246@nyu.edu>
2021-01-11 11:55:58 -0500

    tools is now a submodule

--- plenoptic/__init__.py
+++ plenoptic/__init__.py
@@ -1,18 +1,6 @@
 from . import simulate as simul
 from . import synthesize as synth
 from . import metric
+from . import tools
 # from . import learn as learn
-
-from .tools.data import *
-from .tools.conv import *
-from .tools.signal import *
-from .tools.stats import *
-from .tools.display import *
-from .tools.straightness import *
-
-from .tools import optim
-from .tools import external
-
-from .tools.metamer_utils import *
-
 from .version import version as __version__
