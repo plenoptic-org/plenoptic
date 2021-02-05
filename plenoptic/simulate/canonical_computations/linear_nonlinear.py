1602eaa3b0cac4e1c5785368f9bc4e3605787f32
pehf <pef246@nyu.edu>
2021-02-05 15:31:46 -0500

    better arguments

--- plenoptic/simulate/canonical_computations/linear_nonlinear.py
+++ plenoptic/simulate/canonical_computations/linear_nonlinear.py
@@ -11,7 +11,7 @@ class Linear_Nonlinear(nn.Module):
 
     Notes
     -----
-    Cascades of linear filters and simple nonlinearities are
+    (cascades of) linear filters and simple nonlinearities are
     surprisingly effective at performing visual tasks and at capturing
     cell responses in the early visual system:
     - the linear filters capture basic cell properties (tuning/invariance)
@@ -22,34 +22,38 @@ class Linear_Nonlinear(nn.Module):
     Parameters
     ----------
     n_channels: int, optional
-        number of channels, by default three: low pass, high pass on / off.
+        number of convolutional channels
     kernel_size : tuple of two ints
-        size of the receptive field (odd size supported), [3x3] by default
-    variance: int, optional
+        size of the receptive fields (odd size supported), [3x3] by default
+    default_filters : bool, optional
+        Initialize the first three filters to: low pass, high pass on / off.
     """
-    def __init__(self, n_channels=3, kernel_size=(3, 3), variance=3):
+    def __init__(self, n_channels=3, kernel_size=(3, 3), default_filters=True):
         super().__init__()
 
         self.conv = nn.Conv2d(1, n_channels, kernel_size,
                               padding=[int((k-1)/2) for k in kernel_size],
                               bias=False)
 
-        xs = torch.linspace(-2, 2, kernel_size[0])
-        g1 = torch.exp(-xs**2 / (2 * variance))
-        f1 = torch.outer(g1, g1)
-        f1 = f1 / f1.sum()
+        if default_filters and n_channels >= 3:
+            variance = 3
+            xs = torch.linspace(-2, 2, kernel_size[0])
 
-        g2 = torch.exp(-xs**2 / (variance / 3))
-        f2 = torch.outer(g2, g2)
-        f2 = f2 / f2.sum() - f1
-        f2 = f2 - f2.sum()
+            g1 = torch.exp(-xs**2 / (2 * variance))
+            f1 = torch.outer(g1, g1)
+            f1 = f1 / f1.sum()
 
-        f1 = torch.tensor(f1, dtype=torch.float32)
-        f2 = torch.tensor(f2, dtype=torch.float32)
+            g2 = torch.exp(-xs**2 / (variance / 3))
+            f2 = torch.outer(g2, g2)
+            f2 = f2 / f2.sum() - f1
+            f2 = f2 - f2.sum()
 
-        self.conv.weight.data[0, 0] = nn.Parameter(f1)
-        self.conv.weight.data[1, 0] = nn.Parameter(f2)
-        self.conv.weight.data[2, 0] = nn.Parameter(-f2)
+            f1 = torch.tensor(f1, dtype=torch.float32)
+            f2 = torch.tensor(f2, dtype=torch.float32)
+
+            self.conv.weight.data[0, 0] = nn.Parameter(f1)
+            self.conv.weight.data[1, 0] = nn.Parameter(f2)
+            self.conv.weight.data[2, 0] = nn.Parameter(-f2)
 
         self.relu = nn.ReLU()
 
