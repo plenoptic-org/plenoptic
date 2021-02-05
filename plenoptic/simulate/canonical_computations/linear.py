61b8f411c5a3df8af9722e76c2884a77efaea6c6
pehf <pef246@nyu.edu>
2021-02-05 17:58:48 -0500

    fix some notebooks and document linear.py

--- plenoptic/simulate/canonical_computations/linear.py
+++ plenoptic/simulate/canonical_computations/linear.py
@@ -1,21 +1,40 @@
 import torch
 import torch.nn as nn
-from pyrtools.tools.synthetic_images import gaussian
 
 
 class Linear(nn.Module):
-
-    def __init__(self, kernel_size=(7, 7)):
+    """Simplistic linear convolutional model:
+    It splits the input greyscale image into low and high frequencies.
+    """
+    def __init__(self):
         super().__init__()
 
-        self.conv1 = nn.Conv2d(1, 2, kernel_size=kernel_size, padding=(0, 0), bias=False)
-        f1 = gaussian(kernel_size, covariance=2)
-        f2 = gaussian(kernel_size, covariance=1) - f1
-        self.conv1.weight.data[0,0] = nn.Parameter(torch.tensor(f1, dtype=torch.float32))
-        self.conv1.weight.data[1,0] = nn.Parameter(torch.tensor(f2, dtype=torch.float32))
+        kernel_size = (3, 3)
+
+        self.conv = nn.Conv2d(1, 2, kernel_size,
+                              padding=(1, 1),
+                              bias=False)
+
+        variance = 3
+        xs = torch.linspace(-2, 2, kernel_size[0])
+
+        g1 = torch.exp(-xs**2 / (2 * variance))
+        f1 = torch.outer(g1, g1)
+        f1 = f1 / f1.sum()
+
+        g2 = torch.exp(-xs**2 / (variance / 3))
+        f2 = torch.outer(g2, g2)
+        f2 = f2 / f2.sum() - f1
+        f2 = f2 - f2.sum()
+
+        f1 = torch.tensor(f1, dtype=torch.float32)
+        f2 = torch.tensor(f2, dtype=torch.float32)
+
+        self.conv.weight.data[0, 0] = nn.Parameter(f1)
+        self.conv.weight.data[1, 0] = nn.Parameter(f2)
 
     def forward(self, x):
 
-        h = self.conv1(x)
+        h = self.conv(x)
 
         return h
