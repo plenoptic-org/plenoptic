82071f6566e74b343fa030c77fe85bdfc04db14d
pehf <pef246@nyu.edu>
2021-03-18 17:19:57 -0400

    move polar_pyr to my private repo

--- plenoptic/simulate/models/__init__.py
+++ plenoptic/simulate/models/__init__.py
@@ -3,4 +3,4 @@ from .texture_statistics import Texture_Statistics
 from .spectral import Spectral
 from .cross_spectral import CrossSpectral
 from .ventral_stream import PooledVentralStream, PooledV1, PooledRGC
-from .polar_pyramid import Polar_Pyramid
+# from .polar_pyramid import Polar_Pyramid
