00f4036e65248753d2374378092aa6ddc2925bbb
pehf <pef246@nyu.edu>
2021-04-14 18:53:55 -0400

    rethinking what deviation from line can tell us, and initializers

--- plenoptic/tools/straightness.py
+++ plenoptic/tools/straightness.py
@@ -2,35 +2,39 @@ import torch
 
 
 def make_straight_line(start, stop, n_steps):
-    """
+    """make a straight line between `start` and `stop`
+    made up of `n_steps`
+
     Parameters
     ----------
-    start (resp. stop ): torch.Tensor
-        signal of shape [1, C, H, W]
-        the pylons between which to draw the line
+    start, stop: torch.Tensor
+        signal of shape [1, D], the anchor points between which
+        a line will be made
     n_steps : int
         number of steps
 
     Returns
     -------
     straight : torch.Tensor
-        sequence of shape [n_steps+1, C, H, W]
+        sequence of shape [n_steps+1, D]
     """
     assert start.shape == stop.shape
     assert start.shape[0] == 1
-    tt = torch.linspace(0, 1, steps=n_steps+1).view(n_steps+1, 1, 1, 1)
+    tt = torch.linspace(0, 1, steps=n_steps+1).view(n_steps+1, 1)
     straight = (1 - tt) * start + tt * stop
 
     return straight
 
 
 def sample_brownian_bridge(start, stop, n_steps, max_norm=1):
-    """
+    """Sample a brownian bridge between `start` and `stop` made up of `n_steps`
+
     Parameters
     ----------
-    start (resp. stop): torch.Tensor
-        signal of shape [1, C, H, W]
-        the pylons on which to build the bridge
+    start, stop: torch.Tensor
+        signal of shape [1, D], the anchor points between which
+        a random path will be sampled (like pylons on which
+        the bridge will rest)
     n_steps: int
         number of steps on the bridge
     max_norm: float
@@ -44,58 +48,80 @@ def sample_brownian_bridge(start, stop, n_steps, max_norm=1):
     Returns
     -------
     B: torch.Tensor
-        sequence of shape [n_steps+1, C, H, W]
+        sequence of shape [n_steps+1, D]
         a brownian bridge accros the two pylons
     """
     assert start.shape == stop.shape
     assert start.shape[0] == 1
     assert max_norm >= 0
 
-    d = start.numel()
+    D = start.shape[1]
     dt = torch.tensor(1/n_steps)
     tt = torch.linspace(0, 1, steps=n_steps+1)[:, None]
 
-    sigma = torch.sqrt(dt / d) * 2. * max_norm
-    dW = sigma * torch.randn(n_steps+1, d)
+    sigma = torch.sqrt(dt / D) * 2. * max_norm
+    dW = sigma * torch.randn(n_steps+1, D)
     dW[0] = start.flatten()
-    W = torch.cumsum(dW, axis=0)
+    W = torch.cumsum(dW, dim=0)
 
-    B = W - tt * (W[-1] - stop.flatten())[None, :]
-    B = torch.reshape(B, (n_steps+1, *start.shape[1:]))
+    B = W - tt * (W[-1:] - stop)
+
     return B
 
 
-def distance_from_line(y, y0=None, y1=None):
-    """Compute the l2 distance of each y to its projection onto the
-    straight line going accross the pylons y0 to y1.
+def deviation_from_line(y, y0=None, y1=None, normalize=True):
+    """Compute the deviation of `y` to the straight line between `y0` and `y1`
+    for visualization purposes.
+
+    Project each point of the path `y` onto the line defined by
+    the anchor points, and measure the two sides of a right triangle:
+    - from the projected point to the first anchor point
+      (aka. distance along line)
+    - from the projected point to the corresponding point on the path `y`
+      (aka. distance from line).
 
     Parameters
     ----------
     y: torch.FloatTensor
-        sequence of signals of shape [T, C, H, W]
-    y0 (resp. y1): torch.Tensor, optional
-        signal of shape [1, C, H, W] set by default to the endpoints of y
+        sequence of signals of shape [T, D]
+    y0, y1: torch.Tensor, optional
+        signal of shape [1, D], the points that define the line
+        set by default to the endpoints of y
+    normalize: bool, optional
+        use the distance between the anchor points as a unit of measurement
 
     Returns
     -------
-    dist: torch.FloatTensor
+    dist_along_line: torch.FloatTensor
+        sequence of T euclidian distances along the line
+
+    dist_from_line: torch.FloatTensor
         sequence of T euclidian distances to the line
     """
+    T, D = y.shape
     if y0 is None:
-        y0 = y[0:1]
+        y0 = y[0].view(1, D)
     if y1 is None:
-        y1 = y[-1:]
+        y1 = y[-1].view(1, D)
     assert y0.shape == y1.shape
-    assert y0.shape == y[0:1].shape
 
-    line = make_straight_line(y0, y1, len(y)-1)
-    dist = (y - line).pow(2).sum(dim=(1, 2, 3)).pow(.5)
+    line = (y1 - y0)
+    line_length = torch.norm(line)
+    line = line / line_length
+    y_centered = y - y0
+    dist_along_line = y_centered @ line[0]
+    projection = dist_along_line.view(T, 1) * line
+    dist_from_line = torch.norm(y_centered - projection, dim=1)
 
-    return dist
+    if normalize:
+        dist_along_line /= line_length
+        dist_from_line /= line_length
+
+    return dist_along_line, dist_from_line
 
 
 def translation_sequence(image, n_steps=10):
-    """horizontal translation
+    """make a horizontal translation sequence on `image`
 
     Parameters
     ---------
@@ -109,7 +135,6 @@ def translation_sequence(image, n_steps=10):
     -------
     sequence: torch.Tensor
         [T, C, H, W], with T = n_steps + 1
-
     """
     sequence = torch.empty(n_steps+1, *image.shape)
 
