3c759199b6d8727395b37131b131049d9c511148
William F. Broderick <billbrod@gmail.com>
2021-05-11 17:15:42 -0400

    corrects spacing in convert_float_to_int exceptions

--- plenoptic/tools/data.py
+++ plenoptic/tools/data.py
@@ -144,7 +144,7 @@ def convert_float_to_int(im, dtype=np.uint8):
 
     """
     if im.max() > 1:
-        raise Exception("all values of im must lie between 0 and 1,"
+        raise Exception("all values of im must lie between 0 and 1, "
                         f"but max is {im.max()}")
     return (im * np.iinfo(dtype).max).astype(dtype)
 
@@ -162,8 +162,8 @@ def torch_complex_to_numpy(x):
 
     x_np = to_numpy(x)
     if x.ndim not in [5, 6]:
-        raise Exception(f"x has {x.ndim} dimensions, but a complex tensor"
-                        "should have 5 (real and imaginary stacked along"
+        raise Exception(f"x has {x.ndim} dimensions, but a complex tensor "
+                        "should have 5 (real and imaginary stacked along "
                         "the final dim) or 6 if it's a video!")
     x_np = x_np[..., 0] + 1j * x_np[..., 1]
     return x_np
