64586ec99e20451ad9e93456f7fe6c11480d1b16
pehf <pef246@nyu.edu>
2021-05-27 19:04:57 -0400

    a little more testing

--- tests/test_geodesic.py
+++ tests/test_geodesic.py
@@ -29,16 +29,60 @@ class TestGeodesic(object):
     @pytest.mark.parametrize("optimizer", ["Adam", "SGD"])
     @pytest.mark.parametrize("learning_rate", [.001, .01])
     @pytest.mark.parametrize("n_steps", [5, 10])
-    def test_geodesic_texture(self, einstein_img_small, init, optimizer, learning_rate, n_steps):
+    def test_geodesic_texture(self, einstein_img_small, init, optimizer,
+                              learning_rate, n_steps):
+
+        sequence = po.tools.translation_sequence(einstein_img_small[0],
+                                                 n_steps)
 
         model = po.simul.OnOff(kernel_size=(31, 31), pretrained=True)
-        sequence = po.tools.translation_sequence(einstein_img_small[0], n_steps)
         moog = po.synth.Geodesic(sequence[0:1], sequence[-1:],
                                  model, n_steps, init)
         if optimizer == "SGD":
             optimizer = torch.optim.SGD([moog.x], lr=learning_rate)
         moog.synthesize(max_iter=5, learning_rate=learning_rate,
-                                    optimizer=optimizer)
+                        optimizer=optimizer)
+
         moog.plot_loss()
         moog.plot_deviation_from_line(video=sequence)
         moog.calculate_jerkiness()
+
+    def test_conditional_geodesic(self, einstein_img_small):
+        n_steps = 10
+        sequence = po.tools.translation_sequence(einstein_img_small[0],
+                                                 n_steps)
+        vid = po.tools.rescale(sequence, 0, 1)
+        imgA = vid[0:1]
+        imgB = vid[-1:]
+
+        model = po.simul.OnOff(kernel_size=(31, 31), pretrained=True)
+        moog_conditional = po.synth.Geodesic(imgA, imgB, model, n_steps,
+                                             init='bridge')
+        moog_conditional.synthesize(max_iter=100, conditional=True,
+                                    regularized=False, tol=None)
+
+        print("last losses ", moog_conditional.loss[-1])
+
+        moog_conditional.plot_loss()
+        moog_conditional.plot_deviation_from_line(vid)
+        moog_conditional.calculate_jerkiness()
+
+    def test_regularized_geodesic(self, einstein_img_small):
+        n_steps = 10
+        sequence = po.tools.translation_sequence(einstein_img_small[0],
+                                                 n_steps)
+        vid = po.tools.rescale(sequence, 0, 1)
+        imgA = vid[0:1]
+        imgB = vid[-1:]
+
+        model = po.simul.OnOff(kernel_size=(31, 31), pretrained=True)
+        moog_regularized = po.synth.Geodesic(imgA, imgB, model, n_steps,
+                                             init='bridge')
+        moog_regularized.synthesize(max_iter=100, conditional=False,
+                                    regularized=True, tol=0)
+
+        moog_regularized.plot_loss()
+        moog_regularized.plot_deviation_from_line(vid)
+        moog_regularized.calculate_jerkiness()
+
+    # def test_nested_geodesic(self):
