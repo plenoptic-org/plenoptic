68790e7f03cc503d5dce6099114521558ea7775a
William F. Broderick <billbrod@gmail.com>
2023-01-30 17:34:52 -0500

    updates geodesic test for new structure

--- tests/test_geodesic.py
+++ tests/test_geodesic.py
@@ -1,7 +1,8 @@
 import plenoptic as po
+import numpy as np
 import pytest
 import torch
-from conftest import DATA_DIR, DEVICE
+from conftest import DEVICE
 
 
 class TestSequences(object):
@@ -12,10 +13,12 @@ class TestSequences(object):
         set the seed manually."""
         torch.manual_seed(0)
         t = 2**6
-        d = 2**15
-        b = po.tools.sample_brownian_bridge(torch.randn(1, d),
-                                      torch.randn(1, d),
-                                      t, d**.5)
+        d = 2**14
+        sqrt_d = int(np.sqrt(d))
+        start = torch.randn(1, d).reshape(1, 1, sqrt_d, sqrt_d)
+        stop = torch.randn(1, d).reshape(1, 1, sqrt_d, sqrt_d)
+        b = po.tools.sample_brownian_bridge(start, stop,
+                                            t, d**.5)
         a, f = po.tools.deviation_from_line(b)
         assert torch.abs(a[t//2] - .5) < 1e-2, f"{a[t//2]}"
         assert torch.abs(f[t//2] - 2**.5/2) < 1e-2, f"{f[t//2]}"
@@ -24,21 +27,20 @@ class TestSequences(object):
 class TestGeodesic(object):
 
     @pytest.mark.parametrize("init", ["straight", "bridge"])
-    @pytest.mark.parametrize("optimizer", ["Adam", "SGD"])
-    @pytest.mark.parametrize("learning_rate", [.001, .01])
+    @pytest.mark.parametrize("optimizer", [None, "SGD"])
     @pytest.mark.parametrize("n_steps", [5, 10])
-    def test_geodesic_texture(self, einstein_img_small, init, optimizer, learning_rate, n_steps):
+    def test_geodesic_texture(self, einstein_img_small, init, optimizer, n_steps):
 
         model = po.simul.OnOff(kernel_size=(31, 31), pretrained=True)
-        sequence = po.tools.translation_sequence(einstein_img_small[0], n_steps)
+        po.tools.remove_grad(model)
+        sequence = po.tools.translation_sequence(einstein_img_small, n_steps)
         moog = po.synth.Geodesic(sequence[0:1], sequence[-1:],
                                  model, n_steps, init)
         if optimizer == "SGD":
-            optimizer = torch.optim.SGD([moog.x], lr=learning_rate)
-        moog.synthesize(max_iter=5, learning_rate=learning_rate,
-                                    optimizer=optimizer)
-        moog.plot_loss()
-        moog.plot_deviation_from_line(video=sequence)
+            optimizer = torch.optim.SGD([moog._geodesic], lr=.01)
+        moog.synthesize(max_iter=5, optimizer=optimizer)
+        po.synth.geodesic.plot_loss(moog)
+        po.synth.geodesic.plot_deviation_from_line(moog, natural_video=sequence)
         moog.calculate_jerkiness()
 
 
