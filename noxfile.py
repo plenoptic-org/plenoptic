58d7c2b1fe8a111d9770e0bbc137aa02ba88b8cc
BalzaniEdoardo <edoardo.balzani87@gmail.com>
2024-02-21 17:23:12 -0500

    added ruff and nox.

---
+++ noxfile.py
@@ -0,0 +1,14 @@
+import nox
+
+
+@nox.session(name="Run Tests", python=["3.10", "3.11"])
+def tests(session):
+    """Run the test suite."""
+    session.run("pytest")
+
+
+@nox.session(name="linter", python = ["3.10", "3.11"])
+def linters(session):
+    """Run linters"""
+    session.run("ruff", "check", "--ignore", "D")
+
