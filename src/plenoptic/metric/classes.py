6144681e218dea25c1a8e8b8db80f49aa33963f5
hmd101 <33073354+hmd101@users.noreply.github.com>
2024-06-27 21:51:11 -0400

    Update src/plenoptic/metric/classes.py
    Co-authored-by: William F. Broderick <billbrod@gmail.com>

--- src/plenoptic/metric/classes.py
+++ src/plenoptic/metric/classes.py
@@ -11,10 +11,8 @@ class NLP(torch.nn.Module):
 
     NOTE: synthesis using this class will not be the exact same as
     synthesis using the ``plenoptic.metric.nlpd`` function (by default),
-    because the synthesis methods use ``torch.linalg.norm(x - y, ord=2)`` as the
-    distance metric between representations, whereas ``nlpd`` uses the
-    root-mean square of the distance (i.e.,
-    ``torch.sqrt(torch.mean(x-y)**2))``
+    because the ``nlpd`` function uses the root-mean square of the L2 distance (i.e.,
+    ``torch.sqrt(torch.mean(x-y)**2))`` as the distance metric between representations.
 
     """
     def __init__(self):
