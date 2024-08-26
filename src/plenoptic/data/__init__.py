d8c7c96cb342a24eb78e7805dcd69187c6e2e329
William F. Broderick <billbrod@gmail.com>
2024-08-26 16:32:38 -0400

    simplify how we get the images

--- src/plenoptic/data/__init__.py
+++ src/plenoptic/data/__init__.py
@@ -1,28 +1,32 @@
 from . import data_utils
 from .fetch import fetch_data, DOWNLOADABLE_FILES
 import torch
+from importlib import resources
+from ..tools.data import load_images
 
 __all__ = ['einstein', 'curie', 'parrot', 'reptile_skin',
            'color_wheel', 'fetch_data', 'DOWNLOADABLE_FILES']
 def __dir__():
     return __all__
 
-
-def einstein() -> torch.Tensor:
-    return data_utils.get('einstein')
+FILES = resources.files(__name__)
 
 
-def curie() -> torch.Tensor:
-    return data_utils.get('curie')
+def einstein(as_gray: bool = True) -> torch.Tensor:
+    return load_images(FILES / 'einstein.pgm', as_gray=as_gray)
+
+
+def curie(as_gray: bool = True) -> torch.Tensor:
+    return load_images(FILES / 'curie.pgm', as_gray=as_gray)
 
 
 def parrot(as_gray: bool = False) -> torch.Tensor:
-    return data_utils.get('parrot', as_gray=as_gray)
+    return load_images(FILES / 'parrot.png', as_gray=as_gray)
 
 
-def reptile_skin() -> torch.Tensor:
-    return data_utils.get('reptile_skin')
+def reptile_skin(as_gray: bool = True) -> torch.Tensor:
+    return load_images(FILES / 'reptile_skin.pgm', as_gray=as_gray)
 
 
 def color_wheel(as_gray: bool = False) -> torch.Tensor:
-    return data_utils.get('color_wheel', as_gray=as_gray)
+    return load_images(FILES / 'color_wheel.jpg', as_gray=as_gray)
