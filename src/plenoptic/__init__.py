a4e2f57c3f469b53a9c19355a72cb2b539f014c4
Hanna Dettki <hanna.dettki@gmx.de>
2024-09-28 13:30:55 -0400

    tests run again and circular import error resolved

--- src/plenoptic/__init__.py
+++ src/plenoptic/__init__.py
@@ -1,8 +1,12 @@
 # ignore F401 (unused import)
 # ruff: noqa: F401
-from . import data, metric, tools
+# ruff: noqa: I001 (isort) import order matters to avoid circular dependencies and tests to fail
+
 from . import simulate as simul
 from . import synthesize as synth
+
+# needs to be imported after simulate and synthesize and before tools:
+from . import data, metric, tools
 from .tools.data import load_images, to_numpy
 from .tools.display import animshow, imshow, pyrshow
 from .version import version as __version__
