9975ef0b655381e3109de8d31a5aca47c767883e
Hanna Dettki <hanna.dettki@gmx.de>
2024-09-28 14:55:56 -0400

    adding missing import conv to init in tools

--- src/plenoptic/tools/__init__.py
+++ src/plenoptic/tools/__init__.py
@@ -1,6 +1,7 @@
 # ignore F401 (unused import) and F403 (import * is bad practice)
 # ruff: noqa:  F401, F403
 from . import validate
+from .conv import *
 from .data import *
 from .display import *
 from .external import *
