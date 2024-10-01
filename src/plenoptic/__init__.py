fb2f5a04a8c52f9e558c66416eaac95623f5f3e2
Hanna Dettki <hanna.dettki@gmx.de>
2024-10-01 13:40:58 -0400

    imports sorted in init

--- src/plenoptic/__init__.py
+++ src/plenoptic/__init__.py
@@ -1,12 +1,9 @@
 # ignore F401 (unused import)
 # ruff: noqa: F401
-# ruff: noqa: I001 (isort) import order matters to avoid circular dependencies and tests to fail
 
+from . import data, metric, tools
 from . import simulate as simul
 from . import synthesize as synth
-
-# needs to be imported after simulate and synthesize and before tools:
-from . import data, metric, tools
 from .tools.data import load_images, to_numpy
 from .tools.display import animshow, imshow, pyrshow
 from .version import version as __version__
