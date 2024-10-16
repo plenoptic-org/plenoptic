b8d37de16c4304e5458dbe11628d1f83ccfcdc33
Hanna Dettki <hanna.dettki@gmx.de>
2024-10-15 20:14:41 -0400

    added explicit isort skips to imports which need to be in a specific order

--- src/plenoptic/__init__.py
+++ src/plenoptic/__init__.py
@@ -1,10 +1,11 @@
 # ruff: noqa: F401
+# ruff: noqa: I001
 
-# ruff: noqa: I001 (isort) import order matters to avoid circular dependencies
+# Import order matters here to avoid circular dependencies
 from . import simulate as simul  # isort: skip
 from . import synthesize as synth  # isort: skip
 
-# remaining imports
+# Remaining imports
 from . import data, metric, tools
 from .tools.data import load_images, to_numpy
 from .tools.display import animshow, imshow, pyrshow
