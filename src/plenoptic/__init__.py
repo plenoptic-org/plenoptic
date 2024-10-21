f4f136563433d62a0adf7205bfb9246eebf5717b
William F. Broderick <billbrod@gmail.com>
2024-10-21 10:50:27 -0400

    remove unnecessary isort:skip

--- src/plenoptic/__init__.py
+++ src/plenoptic/__init__.py
@@ -1,11 +1,9 @@
 # ruff: noqa: F401
 # ruff: noqa: I001
-
 # Import order matters here to avoid circular dependencies
-from . import simulate as simul  # isort: skip
-from . import synthesize as synth  # isort: skip
 
-# Remaining imports
+from . import simulate as simul
+from . import synthesize as synth
 from . import data, metric, tools
 from .tools.data import load_images, to_numpy
 from .tools.display import animshow, imshow, pyrshow
