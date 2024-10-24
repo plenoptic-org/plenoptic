4a795975387c9c5eeb9efe50165858b9a4516632
William F. Broderick <billbrod@gmail.com>
2024-10-24 10:52:53 -0400

    remove duplicate `exclude_patterns` (#300)

--- docs/conf.py
+++ docs/conf.py
@@ -62,11 +62,6 @@ extensions = [
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
