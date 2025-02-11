dd8c3bec3519ec1d79aa60a1c500a81b6a775016
William F. Broderick <billbrod@gmail.com>
2025-02-11 12:17:05 -0500

    mark known things as safe

--- src/plenoptic/__init__.py
+++ src/plenoptic/__init__.py
@@ -8,3 +8,23 @@ from . import data, metric, tools
 from .tools.data import load_images, to_numpy
 from .tools.display import animshow, imshow, pyrshow
 from .version import version as __version__
+
+import torch
+import collections
+
+# mark the optimizers and learning rate schedulers from torch as safe
+optims = [
+    opt
+    for opt in torch.optim.Optimizer.__subclasses__()
+    if "torch.optim" in opt.__module__
+]
+scheds = [
+    sch
+    for sch in torch.optim.lr_scheduler.LRScheduler.__subclasses__()
+    if "torch.optim.lr_scheduler" in sch.__module__
+]
+# mark the loss functions from plenoptic as safe
+plenoptic_funcs = [eval(f"tools.optim.{f}") for f in tools.optim.OPTIM_FUNCS]
+torch.serialization.add_safe_globals(
+    [dict, collections.defaultdict, *optims, *scheds, *plenoptic_funcs]
+)
--- src/plenoptic/tools/optim.py
+++ src/plenoptic/tools/optim.py
@@ -4,6 +4,8 @@ import numpy as np
 import torch
 from torch import Tensor
 
+OPTIM_FUNCS = ["mse", "l2_norm", "relative_MSE", "penalize_range"]
+
 
 def set_seed(seed: int | None = None) -> None:
     """Set the seed.
