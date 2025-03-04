739fca7fd4423738545e075b0cced44333d23b23
sjvenditto <sjvenditto@gmail.com>
2025-03-04 14:54:52 -0500

    example for metric.mse

--- src/plenoptic/metric/naive.py
+++ src/plenoptic/metric/naive.py
@@ -29,5 +29,14 @@ def mse(img1, img2):
     mse : torch.float
         the mean-squared error between ``img1`` and ``img2``
 
+    Examples
+    --------
+    >>> import plenoptic as po
+    >>> import torch
+    >>> einstein = po.data.einstein()
+    >>> po.tools.set_seed(0)
+    >>> einstein_noisy = einstein + 0.1 * torch.randn_like(einstein)
+    >>> po.metric.mse(einstein, einstein_noisy)
+    tensor([[0.0100]])
     """
     return torch.pow(img1 - img2, 2).mean((-1, -2))
