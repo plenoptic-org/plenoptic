b8f4fdc0489dc516d16ba4df11ae5c5dd81e9c01
William F. Broderick <billbrod@gmail.com>
2025-03-20 13:02:59 -0400

    use plot directive

--- src/plenoptic/data/__init__.py
+++ src/plenoptic/data/__init__.py
@@ -38,14 +38,16 @@ def einstein(as_gray: bool = True) -> torch.Tensor:
 
     Examples
     --------
+    .. plot::
+
+      >>> import plenoptic as po
+      >>> einstein = po.data.einstein()
+      >>> einstein.shape
+      torch.Size([1, 1, 256, 256])
+      >>> po.imshow(einstein) #doctest: +ELLIPSIS
+      <PyrFigure size ...>
+
     >>> import plenoptic as po
-    >>> einstein = po.data.einstein()
-    >>> einstein.shape
-    torch.Size([1, 1, 256, 256])
-
-    >>> po.imshow(einstein)
-    <PyrFigure size 512x640 with 1 Axes>
-
     >>> einstein_rgb = po.data.einstein(as_gray=False)
     >>> einstein_rgb.shape
     torch.Size([1, 3, 256, 256])
@@ -69,14 +71,16 @@ def curie(as_gray: bool = True) -> torch.Tensor:
 
     Examples
     --------
+    .. plot::
+
+      >>> import plenoptic as po
+      >>> curie = po.data.curie()
+      >>> curie.shape
+      torch.Size([1, 1, 256, 256])
+      >>> po.imshow(curie) #doctest: +ELLIPSIS
+      <PyrFigure size ...>
+
     >>> import plenoptic as po
-    >>> curie = po.data.curie()
-    >>> curie.shape
-    torch.Size([1, 1, 256, 256])
-
-    >>> po.imshow(curie)
-    <PyrFigure size 512x640 with 1 Axes>
-
     >>> curie_rgb = po.data.curie(as_gray=False)
     >>> curie_rgb.shape
     torch.Size([1, 3, 256, 256])
@@ -100,14 +104,16 @@ def parrot(as_gray: bool = True) -> torch.Tensor:
 
     Examples
     --------
+    .. plot::
+
+      >>> import plenoptic as po
+      >>> parrot = po.data.parrot()
+      >>> parrot.shape
+      torch.Size([1, 1, 254, 266])
+      >>> po.imshow(parrot) #doctest: +ELLIPSIS
+      <PyrFigure size ...>
+
     >>> import plenoptic as po
-    >>> parrot = po.data.parrot()
-    >>> parrot.shape
-    torch.Size([1, 1, 254, 266])
-
-    >>> po.imshow(parrot)
-    <PyrFigure size 532x634 with 1 Axes>
-
     >>> parrot_rgb = po.data.parrot(as_gray=False)
     >>> parrot_rgb.shape
     torch.Size([1, 3, 254, 266])
@@ -131,14 +137,16 @@ def reptile_skin(as_gray: bool = True) -> torch.Tensor:
 
     Examples
     --------
+    .. plot::
+
+      >>> import plenoptic as po
+      >>> reptile_skin = po.data.reptile_skin()
+      >>> reptile_skin.shape
+      torch.Size([1, 1, 256, 256])
+      >>> po.imshow(reptile_skin) #doctest: +ELLIPSIS
+      <PyrFigure size ...>
+
     >>> import plenoptic as po
-    >>> reptile_skin = po.data.reptile_skin()
-    >>> reptile_skin.shape
-    torch.Size([1, 1, 256, 256])
-
-    >>> po.imshow(reptile_skin)
-    <PyrFigure size 512x640 with 1 Axes>
-
     >>> reptile_skin_rgb = po.data.reptile_skin(as_gray=False)
     >>> reptile_skin_rgb.shape
     torch.Size([1, 3, 256, 256])
@@ -162,20 +170,23 @@ def color_wheel(as_gray: bool = False) -> torch.Tensor:
 
     Examples
     --------
-    >>> import plenoptic as po
-    >>> color_wheel = po.data.color_wheel()
-    >>> color_wheel.shape
-    torch.Size([1, 3, 600, 600])
+    .. plot::
 
-    >>> po.imshow(color_wheel, as_rgb=True)
-    <PyrFigure size 1200x1500 with 1 Axes>
+      >>> import plenoptic as po
+      >>> color_wheel = po.data.color_wheel()
+      >>> color_wheel.shape
+      torch.Size([1, 3, 600, 600])
+      >>> po.imshow(color_wheel, as_rgb=True, zoom=0.5) #doctest: +ELLIPSIS
+      <PyrFigure size ...>
 
-    >>> color_wheel_gray = po.data.color_wheel(as_gray=True)
-    >>> color_wheel_gray.shape
-    torch.Size([1, 1, 600, 600])
+    .. plot::
 
-    >>> po.imshow(color_wheel_gray)
-    <PyrFigure size 1200x1500 with 1 Axes>
+      >>> import plenoptic as po
+      >>> color_wheel_gray = po.data.color_wheel(as_gray=True)
+      >>> color_wheel_gray.shape
+      torch.Size([1, 1, 600, 600])
+      >>> po.imshow(color_wheel_gray, zoom=0.5) #doctest: +ELLIPSIS
+      <PyrFigure size ...>
 
     """
     return load_images(FILES / "color_wheel.jpg", as_gray=as_gray)
