114ae3f8086efa2969ceb02a26af4f603882d2ee
William F. Broderick <billbrod@gmail.com>
2025-04-22 15:20:55 -0400

    adds new linters to noxfile

--- noxfile.py
+++ noxfile.py
@@ -5,6 +5,8 @@ with open(".pre-commit-config.yaml") as f:
     precommit_config = yaml.safe_load(f)
 ruff_version = [r for r in precommit_config["repos"] if "ruff-pre-commit" in r["repo"]]
 ruff_version = ruff_version[0]["rev"].replace("v", "")
+numpydoc_version = [r for r in precommit_config["repos"] if "numpydoc" in r["repo"]]
+numpydoc_version = numpydoc_version[0]["rev"].replace("v", "")
 
 
 @nox.session
@@ -19,8 +21,11 @@ def format(session):
 def lint(session):
     # run linters
     session.install(f"ruff=={ruff_version}")
+    session.install(f"numpydoc=={numpydoc_version}")
     session.run("ruff", "check", "--config=pyproject.toml")
     session.run("ruff", "format", "--check", "--config=pyproject.toml")
+    session.run("numpydoc", "lint", "src")
+    session.run("python", "tests/check_docstrings.py", "src")
 
 
 @nox.session(name="tests", python=["3.10", "3.11", "3.12"])
