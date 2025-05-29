855c7c6d0582d76b851ab711ea6656b4ca92a440
William F. Broderick <billbrod@gmail.com>
2025-05-29 12:08:11 -0400

    actually those quotes are necessary

--- src/plenoptic/tools/convergence.py
+++ src/plenoptic/tools/convergence.py
@@ -28,7 +28,7 @@ if TYPE_CHECKING:
 
 
 def loss_convergence(
-    synth: OptimizedSynthesis,
+    synth: "OptimizedSynthesis",
     stop_criterion: float,
     stop_iters_to_check: int,
 ) -> bool:
@@ -67,7 +67,7 @@ def loss_convergence(
     )
 
 
-def coarse_to_fine_enough(synth: MetamerCTF, i: int, ctf_iters_to_check: int) -> bool:
+def coarse_to_fine_enough(synth: "MetamerCTF", i: int, ctf_iters_to_check: int) -> bool:
     r"""
     Check whether we've been synthesized all scales for long enough.
 
@@ -110,7 +110,7 @@ def coarse_to_fine_enough(synth: MetamerCTF, i: int, ctf_iters_to_check: int) ->
 
 
 def pixel_change_convergence(
-    synth: OptimizedSynthesis,
+    synth: "OptimizedSynthesis",
     stop_criterion: float,
     stop_iters_to_check: int,
 ) -> bool:
