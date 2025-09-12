8725187a193f532d0017d902a2944396994d3dc8
William F. Broderick <billbrod@gmail.com>
2025-09-12 11:05:59 -0400

    fix to() docstring

--- src/plenoptic/synthesize/synthesis.py
+++ src/plenoptic/synthesize/synthesis.py
@@ -399,11 +399,11 @@ class Synthesis(abc.ABC):
             to(tensor, non_blocking=False)
 
         Its signature is similar to :meth:`torch.Tensor.to`, but only accepts
-        floating point desired :attr:`dtype` s. In addition, this method will
-        only cast the floating point parameters and buffers to :attr:`dtype`
+        floating point desired ``dtype``. In addition, this method will
+        only cast the floating point parameters and buffers to ``dtype``
         (if given). The integral parameters and buffers will be moved
-        :attr:`device`, if that is given, but with dtypes unchanged. When
-        :attr:`non_blocking` is set, it tries to convert/move asynchronously
+        ``device``, if that is given, but with dtypes unchanged. When
+        `on_blocking`` is set, it tries to convert/move asynchronously
         with respect to the host if possible, e.g., moving CPU Tensors with
         pinned memory to CUDA devices.
 
