48a1784c9616cd01a7e24c8af58fda5dd6f4b24c
William F. Broderick <billbrod@gmail.com>
2026-01-06 11:10:15 -0500

    tell sphinx to ignore jupyter cache

--- docs/conf.py
+++ docs/conf.py
@@ -80,7 +80,13 @@ master_doc = "index"
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
+exclude_patterns = [
+    "_build",
+    "Thumbs.db",
+    ".DS_Store",
+    "**.ipynb_checkpoints",
+    ".jupyter_cache",
+]
 
 # Napoleon settings
 napoleon_google_docstring = False
