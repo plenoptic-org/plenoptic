e7869465177a64df7d1ffd4a7190bc2a90bcf9ec
William F. Broderick <billbrod@gmail.com>
2026-02-13 16:54:30 -0500

    adds set seed

--- src/plenoptic/__init__.pyi
+++ src/plenoptic/__init__.pyi
@@ -18,6 +18,7 @@ __all__ = [
     "io",
     "optim",
     "validate",
+    "set_seed",
 ]
 
 from . import (
@@ -32,5 +33,6 @@ from . import (
     validate,
 )
 from .data.data import convert_float_to_int, load_images, to_numpy
+from .optim import set_seed
 from .synthesize import Eigendistortion, MADCompetition, Metamer, MetamerCTF
 from .validate import remove_grad
