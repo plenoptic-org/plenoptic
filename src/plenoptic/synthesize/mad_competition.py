a8c5b8810455f1a63e3680d95f17a86bbbccf41a
Daniel Herrera-Esposito <dherrera1911@gmail.com>
2026-03-04 09:51:09 -0500

    Update src/plenoptic/synthesize/mad_competition.py
    Co-authored-by: William F. Broderick <billbrod@gmail.com>

--- src/plenoptic/synthesize/mad_competition.py
+++ src/plenoptic/synthesize/mad_competition.py
@@ -709,7 +709,7 @@ class MADCompetition(OptimizedSynthesis):
         ValueError
             If the saved and loading ``MADCompetition`` objects have a different value
             for any of :attr:`image`, :attr:`penalty_lambda`,
-            :attr:`metric_tradeoff_lambda`, or :attr:`minimax`.
+            :attr:`metric_tradeoff_lambda`, or :attr:`minmax`.
         ValueError
             If the behavior of :attr:`optimized_metric` or :attr:`reference_metric` is
             different between the saved and loading objects.
