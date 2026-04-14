cda61b24f8d3c7a6a1b10cc9b3354119f9ad9b14
William F. Broderick <billbrod@gmail.com>
2026-04-14 16:45:19 -0400

    improve multiline in table
    have the md file correct, and change the rendering to use javascript

--- docs/_static/search-table.js
+++ docs/_static/search-table.js
@@ -9,6 +9,30 @@ document.addEventListener("DOMContentLoaded", function () {
   new DataTable("table.search-table", {
     order: [], // Keep original order
     lengthMenu: [10, 25, { label: "All", value: -1 }],
-    pageLength: 10
+    pageLength: 10,
+    columnDefs: [
+      {
+        targets: 0,
+        render: function(data, type, row, meta) {
+          if (type === "display") {
+            func_name = data.split("plenoptic")
+            pre_tags = func_name[0].replace("<p>", "")
+            post_tags = "</span></code>"
+            func_name = "plenoptic" + func_name[1].split("<")[0]
+            data_display = [func_name]
+            func_name = func_name.split(".")
+            i = func_name.length
+            while (data_display[0].length >= 45) {
+              i = i - 1;
+              data_display = [func_name.slice(0, i).join(".") + ".", func_name.slice(i).join(".")];
+            }
+            data_display = data_display.map((d) => `${pre_tags}${d}${post_tags}`).join(' ')
+            return `<p>${data_display}</p>`
+          } else {
+            return data
+          }
+        }
+      }
+    ]
   });
 });
