713cff9327c138f6b205576824d8d006d9d19b00
William F. Broderick <billbrod@gmail.com>
2026-09-22 09:40:51 -0400

    update doctest

--- src/plenoptic/_synthesize/eigendistortion.py
+++ src/plenoptic/_synthesize/eigendistortion.py
@@ -223,7 +223,7 @@ class Eigendistortion(_Synthesis):
         >>> eig.synthesize("exact")
         >>> # eigenvalue of all eigendistortions
         >>> eig.eigenvalues
-        tensor([1.0000e+00, 7.9990e-01, ..., 1.2294e-08, 1.5311e-08])
+        tensor([1.0000e+00, 7.9990e-01, ...])
         >>> len(eig.eigenvalues)
         1024
 
